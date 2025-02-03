ea05b92da43446c285b8cd2e9937e6da543c24a0
eesha-barad <eeshabarad49@gmail.com>
2025-02-03 21:38:23 +0530

    Created display method to display array

---
+++ ArrayFunctions.java
@@ -0,0 +1,10 @@
+// ArrayFunctions.java
+
+import java.util.*;
+import java.util.Arrays;
+
+public class ArrayFunctions {
+    public void display(int[] array, String type) {
+        System.out.println(type + " numbers: " + Arrays.toString(array));
+    }
+}
