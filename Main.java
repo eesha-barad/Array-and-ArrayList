fe46310f6b86430cd260779dacd1bd8b68760691
eesha-barad <eeshabarad49@gmail.com>
2025-02-03 22:15:04 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,22 @@
 // Main.java
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         UserInput in = new UserInput();
         ArrayFunctions functions = new ArrayFunctions();
         
-        int[] userArray = in.arrayInput();
+        List<Integer> userList = in.arrayInput();
         
         System.out.println("--- Even and Odd Separation ---");
-        functions.oddEven(userArray);
+        functions.oddEven(userList);
         
         System.out.println("\n--- Smallest Distance Between Neighbors ---");
-        int index = functions.findSmallestDistance(userArray);
+        int index = functions.findSmallestDistance(userList);
         System.out.println("Index of first number with smallest distance: " + index);
         
         System.out.println("\n--- Array to ArrayList and Vice Versa ---");
-        functions.convertArrayAndList(userArray);
+        functions.convertArrayAndList(userList);
     }
 }
