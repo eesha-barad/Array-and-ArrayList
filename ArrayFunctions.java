df59c958cad5bb0386c6e0a9c6607722a4b7c2cf
eesha-barad <eeshabarad49@gmail.com>
2025-02-03 21:39:58 +0530

    Created oddEven method to separate odd and even numbers

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -7,4 +7,32 @@ public class ArrayFunctions {
     public void display(int[] array, String type) {
         System.out.println(type + " numbers: " + Arrays.toString(array));
     }
+	
+	public void oddEven(int[] array) {
+        int evenCount = 0, oddCount = 0;
+        
+        for (int num : array) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        
+        int[] even = new int[evenCount];
+        int[] odd = new int[oddCount];
+        evenCount = 0;
+        oddCount = 0;
+        
+        for (int num : array) {
+            if (num % 2 == 0) {
+                even[evenCount++] = num;
+            } else {
+                odd[oddCount++] = num;
+            }
+        }
+        
+        display(even, "Even");
+        display(odd, "Odd");
+    }
 }
