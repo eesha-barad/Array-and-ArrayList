7c9cc059a5d9aad8e868b0a18e676b8bf586ed05
eesha-barad <eeshabarad49@gmail.com>
2025-02-03 21:40:53 +0530

    Created findSmallestDistanceMethod

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -35,4 +35,22 @@ public class ArrayFunctions {
         display(even, "Even");
         display(odd, "Odd");
     }
+	
+	public int findSmallestDistance(int[] array) {
+        if (array.length < 2) {
+            return -1;
+        }
+        
+        int minIndex = 0;
+        int minDistance = Math.abs(array[1] - array[0]);
+        
+        for (int i = 1; i < array.length - 1; i++) {
+            int distance = Math.abs(array[i + 1] - array[i]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
 }
